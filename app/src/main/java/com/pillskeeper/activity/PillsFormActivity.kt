75713a647f1aed2718eef4898dfbcb30618576f9
steconta <stefanocontarino@gmail.com>
2020-03-14 13:31:19 +0100

    added text recognizer e da finire il form

--- app/src/main/java/com/pillskeeper/activity/PillsFormActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/PillsFormActivity.kt
@@ -9,5 +9,6 @@ class PillsFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pills_form)
+        //TODO finire il form
     }
 }

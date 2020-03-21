56fbdc12e988f2b0b7a548179a2d665cb31f9e81
steconta <stefanocontarino@gmail.com>
2020-03-21 16:03:19 +0100

    da sistemaregli spinner

--- app/src/main/java/com/pillskeeper/activity/pills/reminder/ReminderActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/pills/reminder/ReminderActivity.kt
@@ -2,14 +2,18 @@ package com.pillskeeper.activity.pills.reminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.view.get
 import com.pillskeeper.R
 import com.pillskeeper.datamanager.UserInformation
 import com.pillskeeper.enums.RelationEnum
 import kotlinx.android.synthetic.main.activity_new_friend.*
 import kotlinx.android.synthetic.main.activity_reminder.*
 
-class ReminderActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,12 @@ class ReminderActivity : AppCompatActivity() {
 
         spinnerMinutes.adapter = arrayAdapterMinutes
     }
-}
 
-//TODO USARE I CHECKBOX
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(this, parent!!.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show()
+    }
+}

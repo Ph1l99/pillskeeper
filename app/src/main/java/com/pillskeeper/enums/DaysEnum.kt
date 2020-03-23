6f8f164b45d8ada6970cca6beec90ecfa57f76d8
RickyJam <riccardo.pacifico97@gmail.com>
2020-03-23 09:02:41 +0100

    started sorting method

--- app/src/main/java/com/pillskeeper/enums/DaysEnum.kt
+++ app/src/main/java/com/pillskeeper/enums/DaysEnum.kt
@@ -1,11 +1,13 @@
 package com.pillskeeper.enums
 
-enum class DaysEnum {
-    MON,
-    TUE,
-    WED,
-    THU,
-    FRI,
-    SAT,
-    SUN
+enum class DaysEnum(var dayNumber: Int) {
+    MON         (7),
+    TUE         (6),
+    WED         (5),
+    THU         (4),
+    FRI         (3),
+    SAT         (2),
+    SUN         (1)
+
+
 }

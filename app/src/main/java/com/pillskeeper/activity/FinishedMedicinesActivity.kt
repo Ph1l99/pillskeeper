3e2b1fb0fd612e3f521715f56df9d6664b339256
Ph1l <pippo.coglio@gmail.com>
2020-03-29 17:24:30 +0200

    Completed FinishedMedicinesActivity.kt

--- app/src/main/java/com/pillskeeper/activity/FinishedMedicinesActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/FinishedMedicinesActivity.kt
@@ -2,16 +2,49 @@ package com.pillskeeper.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.pillskeeper.R
+import com.pillskeeper.data.LocalMedicine
+import com.pillskeeper.datamanager.LocalDatabase
+import java.util.*
 
 class FinishedMedicinesActivity : AppCompatActivity() {
+
+    companion object {
+        const val MINIMUM_PILLS = 4
+    }
+
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var medicinesListView: ListView
+    private lateinit var arrayMedicines: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finished_medicines)
+        checkAlmostFinishedMedicines()
     }
 
-    private fun checkAlmostFinishedMedicines() {
+    private fun checkAlmostFinishedMedicines(): LinkedList<LocalMedicine> {
+        val list = LocalDatabase.readMedicineList()
+        val outputList = LinkedList(list.filter {
+            it.remainingPills <= MINIMUM_PILLS
+        })
+        displayMedicines(outputList)
+        return outputList
+    }
 
+    private fun displayMedicines(list: LinkedList<LocalMedicine>) {
+        arrayMedicines = ArrayList(list.size)
+        medicinesListView = findViewById(R.id.finishedMedicinesList)
+        for (medicine in list) {
+            arrayMedicines.add(medicine.name)
+        }
+        adapter = ArrayAdapter(
+            this@FinishedMedicinesActivity,
+            android.R.layout.simple_list_item_1,
+            arrayMedicines
+        )
+        medicinesListView.adapter = adapter
     }
 }

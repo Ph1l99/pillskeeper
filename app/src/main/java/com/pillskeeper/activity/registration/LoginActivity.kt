192cf6e6ee287efa300eeaddbb869ca4658a29bc
Ph1l <pippo.coglio@gmail.com>
2020-04-01 15:25:11 +0200

    Fixed LoginActivity.kt

--- app/src/main/java/com/pillskeeper/activity/registration/LoginActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/registration/LoginActivity.kt
@@ -1,10 +1,6 @@
 package com.pillskeeper.activity.registration
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -78,31 +74,4 @@ class LoginActivity : AppCompatActivity() {
                 })
         }
     }
-
-    private fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val nw = connectivityManager.activeNetwork ?: return false
-            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
-            return when {
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                //for other device how are able to connect with Ethernet
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                //for check internet over Bluetooth
-                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-                else -> false
-            }
-        } else {
-            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
-            return nwInfo.isConnected
-        }
-    }
-    /*
-    override fun onResume() {
-        super.onResume()
-        //checkLogin()
-    }
-     */
 }

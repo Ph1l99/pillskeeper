7d16ca8bd814ae137fe54c92a92b5124ed3c2a20
steconta <stefanocontarino@gmail.com>
2020-04-10 16:00:52 +0200

    bozza dei fragment nella home

--- app/src/main/java/com/pillskeeper/activity/homefragments/ReminderAppointmentListFragment.kt
+++ app/src/main/java/com/pillskeeper/activity/homefragments/ReminderAppointmentListFragment.kt
@@ -1,5 +1,6 @@
 package com.pillskeeper.activity.homefragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,9 +10,17 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 
 import com.pillskeeper.R
+import com.pillskeeper.activity.GenericDeleteDialog
+import com.pillskeeper.activity.appointment.AppointmentFormActivity
+import com.pillskeeper.activity.appointment.AppointmentListActivity.Companion.APPOINTMENT_VALUE
 import com.pillskeeper.data.Appointment
+import com.pillskeeper.data.LocalMedicine
+import com.pillskeeper.data.ReminderMedicine
 import com.pillskeeper.data.ReminderMedicineSort
 import com.pillskeeper.datamanager.UserInformation
+import com.pillskeeper.enums.DaysEnum
+import com.pillskeeper.enums.DialogModeEnum
+import com.pillskeeper.enums.MedicineTypeEnum
 import com.pillskeeper.utility.Utils
 import kotlinx.android.synthetic.main.fragment_reminder_appointment_list.*
 import java.util.*
@@ -23,17 +32,33 @@ class ReminderAppointmentListFragment : Fragment() {
     private lateinit var appointmentListMain: ListView
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_reminder_appointment_list, container, false)
 
         appointmentListMain = view.findViewById(R.id.appointmentListMain)
 
+        //TODO da rimuovere
+        funTest()
+
         initList()
+
+        appointmentListMain.setOnItemClickListener { _, _, position, _ ->
+            val intent = Intent(context, AppointmentFormActivity::class.java)
+                .putExtra(APPOINTMENT_VALUE, appointmentListSorted[position])
+            startActivity(intent)
+        }
+
+        appointmentListMain.setOnItemLongClickListener { _, _, position, _ ->
+            context?.let {
+                GenericDeleteDialog(
+                    it,
+                    appointmentListSorted[position].name,
+                    DialogModeEnum.DELETE_APPOINTMENT
+                ).show()
+            }
+            return@setOnItemLongClickListener true
+        }
 
         return view
     }
@@ -74,6 +99,68 @@ class ReminderAppointmentListFragment : Fragment() {
             }
             else -> return ""
         }
+
+    }
+
+    private fun funTest() {
+
+        val days1: LinkedList<DaysEnum> = LinkedList()
+        days1.add(DaysEnum.MON)
+        days1.add(DaysEnum.FRI)
+        days1.add(DaysEnum.SAT)
+        days1.add(DaysEnum.TUE)
+        days1.add(DaysEnum.THU)
+        days1.add(DaysEnum.SUN)
+        days1.add(DaysEnum.WED)
+
+
+        val reminders = LinkedList<ReminderMedicine>()
+        reminders.add(ReminderMedicine(2F, 39, 19, Date(), days1, null, null))
+        reminders.add(ReminderMedicine(1F, 41, 19, Date(), days1, null, null))
+        UserInformation.addNewMedicine(
+            LocalMedicine(
+                "Tachipirina",
+                MedicineTypeEnum.PILLS,
+                24F,
+                7F,
+                reminders,
+                "Tachipirina"
+            )
+        )
+
+        /*val reminders2 = LinkedList<ReminderMedicine>()
+        reminders2.add(ReminderMedicine(1.5F, 0, 13, Date(), days1, null, null))
+        reminders2.add(ReminderMedicine(1F, 0, 11, Date(), days1, null, null))
+        UserInformation.addNewMedicine(
+            LocalMedicine(
+                "Aulin",
+                MedicineTypeEnum.PILLS,
+                24F,
+                24F,
+                null,
+                "Aulin"
+            )
+        )
+        UserInformation.addNewReminderList("Aulin", reminders2)
+
+        UserInformation.addNewAppointment(
+            Appointment("prelieo", Date(), "")
+        )
+        UserInformation.addNewAppointment(
+            Appointment("Visita Urologo", Date(), "")
+        )
+        UserInformation.addNewAppointment(
+            Appointment("Visita Urologo1", Date(), "")
+        )
+        UserInformation.addNewAppointment(
+            Appointment("Visita Urologo2", Date(), "")
+        )
+        UserInformation.addNewAppointment(
+            Appointment("Visita Urologo3", Date(), "")
+        )
+        UserInformation.addNewAppointment(
+            Appointment("Visita Urologo4", Date(), "")
+        )*/
 
     }
 }

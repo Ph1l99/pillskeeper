81096b589a624653b0c5cf251ce1d7bcfa08da36
Ph1l <pippo.coglio@gmail.com>
2020-04-12 15:32:40 +0200

    Fixed FinishedMedicinesActivity.kt

--- app/src/main/java/com/pillskeeper/activity/medicine/FinishedMedicinesActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/medicine/FinishedMedicinesActivity.kt
@@ -91,30 +91,30 @@ class FinishedMedicinesActivity : AppCompatActivity() {
 
         if (localFriendList.isEmpty()) {
             Toast.makeText(this, getText(R.string.addFriend), Toast.LENGTH_LONG).show()
-        }
-
-        for (friend in localFriendList) {
-            listFriends.add(friend.name + " " + friend.surname)
-        }
-        selectableFriends = Array(listFriends.size) { "" }
-        listFriends.toArray(selectableFriends)
-        val selectedItems = Array(listFriends.size) { false }
-
-        val builder = AlertDialog.Builder(this)
-        builder.setMultiChoiceItems(selectableFriends, null) { _, which, isChecked ->
-            selectedItems[which] = isChecked
-        }
-        builder.setPositiveButton(R.string.sendMail) { _, _ ->
-            for (i in selectedItems.indices) {
-                if (selectedItems[i]) {
-                    chosenFriends.add(extractMail(selectableFriends[i], localFriendList))
-                }
+        } else {
+            for (friend in localFriendList) {
+                listFriends.add(friend.name + " " + friend.surname)
             }
-            sendEmail(localMedicine, chosenFriends)
-        }
+            selectableFriends = Array(listFriends.size) { "" }
+            listFriends.toArray(selectableFriends)
+            val selectedItems = Array(listFriends.size) { false }
 
-        val dialog = builder.create()
-        dialog.show()
+            val builder = AlertDialog.Builder(this)
+            builder.setMultiChoiceItems(selectableFriends, null) { _, which, isChecked ->
+                selectedItems[which] = isChecked
+            }
+            builder.setPositiveButton(R.string.sendMail) { _, _ ->
+                for (i in selectedItems.indices) {
+                    if (selectedItems[i]) {
+                        chosenFriends.add(extractMail(selectableFriends[i], localFriendList))
+                    }
+                }
+                sendEmail(localMedicine, chosenFriends)
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 
     private fun extractMail(friend: String, listFriends: LinkedList<Friend>): String {

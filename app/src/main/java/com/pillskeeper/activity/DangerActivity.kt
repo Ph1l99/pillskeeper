091b5d50b7f773d67fe501b371c8ecc8f1cf191b
Ph1l <pippo.coglio@gmail.com>
2020-04-14 16:34:11 +0200

    Added DangerActivity.kt to menu

--- app/src/main/java/com/pillskeeper/activity/DangerActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/DangerActivity.kt
@@ -1,13 +1,29 @@
 package com.pillskeeper.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.pillskeeper.R
+import kotlinx.android.synthetic.main.activity_danger.*
 
 class DangerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_danger)
+
+        deleteAccount.setOnClickListener {
+
+        }
+        resetPassword.setOnClickListener {
+
+        }
+    }
+
+    private fun deleteAccount() {
+
+    }
+
+    private fun resetPassword() {
+
     }
 }

52c033572acb04909da89329daf12773b4532ee0
phil <pippo.coglio@gmail.com>
2020-04-25 08:46:40 +0200

    Added comments

--- app/src/main/java/com/pillskeeper/enums/RelationEnum.kt
+++ app/src/main/java/com/pillskeeper/enums/RelationEnum.kt
@@ -1,5 +1,8 @@
 package com.pillskeeper.enums
 
+/**
+ * Enumerative which represents the type of user that can be added to PillsKeeper
+ */
 enum class RelationEnum(var relation: String) {
     Doctor      ("Doctor"),
     Friend      ("Friend")

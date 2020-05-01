41dbcd92ec8d699019f5a2acc8b3324d6cd9ca0a
Ph1l <pippo.coglio@gmail.com>
2020-05-01 09:21:40 +0200

    Fixed strings.xml

--- app/src/main/java/com/pillskeeper/utility/Mail.kt
+++ app/src/main/java/com/pillskeeper/utility/Mail.kt
@@ -9,7 +9,7 @@ data class Mail(var mailsubject: String, var mailBody: String) {
 
     companion object {
         fun composeMail(medicine: RemoteMedicine, user: User): Mail? {
-            val mSubject: String = context.getString(R.string.mailsubject) + medicine.name
+            val mSubject: String = context.getString(R.string.mailsubject) + medicine.name //TODO
             val mText: String =
                 context.getString(R.string.mailtext) + "Medicina: " + medicine.name + "\nTiplogia: " + medicine.medicineType + "\nUtente: " + user.name + " " + user.surname
             return Mail(mSubject, mText)

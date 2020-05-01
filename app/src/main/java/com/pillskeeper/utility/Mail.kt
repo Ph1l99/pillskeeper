f45b20c0f999c9d4286ed0ce14a8ca0ab3384780
Ph1l <pippo.coglio@gmail.com>
2020-05-01 16:06:16 +0200

    Fixed mail and strings.xml

--- app/src/main/java/com/pillskeeper/utility/Mail.kt
+++ app/src/main/java/com/pillskeeper/utility/Mail.kt
@@ -5,14 +5,20 @@ import com.pillskeeper.data.RemoteMedicine
 import com.pillskeeper.data.User
 import com.pillskeeper.datamanager.UserInformation.context
 
+/**
+ * Data class for sending emails
+ */
 data class Mail(var mailsubject: String, var mailBody: String) {
 
     companion object {
         fun composeMail(medicine: RemoteMedicine, user: User): Mail? {
-            val mSubject: String = context.getString(R.string.mailsubject) + medicine.name //TODO
+            val mSubject: String = context.getString(R.string.mailsubject) + medicine.name
             val sb = StringBuilder()
-            sb.append(context.getString(R.string.mailtext))
-                context.getString(R.string.mailtext) + "Medicina: " + medicine.name + "\nTiplogia: " + medicine.medicineType + "\nUtente: " + user.name + " " + user.surname
+            sb.append(context.getString(R.string.mailtext)).append("\n")
+                .append(context.getString(R.string.medicine)).append(": ").append(medicine.name).append("\n")
+                .append(context.getString(R.string.medType)).append(": ").append(medicine.medicineType).append("\n")
+                .append(context.getString(R.string.user)).append(": ").append(user.name + " " + user.surname).append("\n")
+                .append(context.getString(R.string.status))
             return Mail(mSubject, sb.toString())
         }
     }

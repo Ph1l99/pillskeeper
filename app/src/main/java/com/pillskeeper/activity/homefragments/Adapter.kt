213a87155fdcaf8df8ead991e047dbc2f364a4d2
Ph1l <pippo.coglio@gmail.com>
2020-05-01 14:51:17 +0200

    Fixed Adapter.kt

--- app/src/main/java/com/pillskeeper/activity/homefragments/Adapter.kt
+++ app/src/main/java/com/pillskeeper/activity/homefragments/Adapter.kt
@@ -1,26 +1,22 @@
 package com.pillskeeper.activity.homefragments
 
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.pillskeeper.R
 
-class Adapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+class Adapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-    companion object{
+    companion object {
         const val PILLS_PAGE = 0
         const val APPOINTMENTS_PAGE = 1
     }
 
 
-
     override fun getItem(position: Int): Fragment {
-        return when(position){
+        return when (position) {
             PILLS_PAGE -> ReminderMedicineListFragment()
             APPOINTMENTS_PAGE -> ReminderAppointmentListFragment()
             else -> ReminderMedicineListFragment()
-
         }
     }
 

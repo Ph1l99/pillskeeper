effb0fc02d494a748347beef40a3750cfeec3f9e
Riccardo <riccardo.pacifico97@gmail.com>
2020-05-01 15:12:47 +0200

    refactoring package name

--- app/src/main/java/com/pillskeeper/activity/home/HomeActivity.kt
+++ app/src/main/java/com/pillskeeper/activity/home/HomeActivity.kt
@@ -21,10 +21,7 @@ import com.pillskeeper.utility.Utils
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.view.*
 
-class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
-
-    //private lateinit var appointmentListSorted: LinkedList<Appointment>
-    //private lateinit var reminderListSorted: LinkedList<ReminderMedicineSort>
+class HomeActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
     private lateinit var drawerLayout: DrawerLayout
@@ -44,7 +41,6 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
         setSupportActionBar(toolbar)
 
         UserInformation.context = this
-
         Utils.stdLayout = EditText(this).background
 
         val menu = Menu(toolbar, drawerLayout, navView, this)
@@ -63,13 +59,9 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
 
         /*LISTENERS*/
         tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab?) {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
 
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 if(tab?.position == HomeAdapter.PILLS_PAGE){
@@ -90,77 +82,4 @@ class HomeActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
             startActivity(intent)
         }
     }
-
-    override fun onTabReselected(tab: TabLayout.Tab?) {}
-
-    override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-    override fun onTabSelected(tab: TabLayout.Tab?) {
-        if(tab?.position == HomeAdapter.PILLS_PAGE){
-            viewPager.currentItem = HomeAdapter.PILLS_PAGE
-        } else {
-            viewPager.currentItem = HomeAdapter.APPOINTMENTS_PAGE
-        }
-    }
-
-
-    /*
-    override fun onResume() {
-        super.onResume()
-        initLists()
-    }
-
-
-
-
-    private fun initLists(filterDate: Date = Date()) {
-        filterDate.time = Utils.dataNormalizationLimit(filterDate)
-
-        /*Appointment list*/
-        UserInformation.appointments.sortWith(compareBy { it.date })
-        appointmentListSorted = UserInformation.appointments
-        appointmentListSorted = LinkedList(appointmentListSorted.filter { it.date <= filterDate })
-        val arrayAdapterAppointments = LinkedList<String>()
-        appointmentListSorted.forEach { arrayAdapterAppointments.add(formatOutputString(it)) }
-        appointmentListMain.adapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayAdapterAppointments)
-
-        /*Reminder List*/
-        reminderListSorted = Utils.getSortedListReminders(filterDate)
-        val arrayAdapterReminders = LinkedList<String>()
-        reminderListSorted.forEach { arrayAdapterReminders.add(formatOutputString(it)) }
-        reminderListMain.adapter =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayAdapterReminders)
-
-    }
-
-    private fun formatOutputString(item: Any): String {
-        val cal: Calendar = Calendar.getInstance()
-        when (item) {
-            is ReminderMedicineSort -> {
-                cal.time = item.reminder.startingDay
-                var text =
-                    "${item.medName}  -  ${item.reminder.dosage} ${getText(item.medType.text)} - "
-                text += if (item.reminder.hours < 10) "0${item.reminder.hours}" else item.reminder.hours
-                text += ":"
-                text += if (item.reminder.minutes < 10) "0${item.reminder.minutes}" else item.reminder.minutes
-                text += "  ${cal.get(Calendar.DAY_OF_MONTH)}/${cal.get(Calendar.MONTH) + 1}"
-                return text
-            }
-            is Appointment -> {
-                cal.time = item.date
-                var text =
-                    "${item.name} - ${cal.get(Calendar.DAY_OF_MONTH)}/${cal.get(Calendar.MONTH) + 1}  "
-                text += "${cal.get(Calendar.HOUR_OF_DAY)}:"
-                text += if (cal.get(Calendar.MINUTE) < 10) "0${cal.get(Calendar.MINUTE)}" else cal.get(
-                    Calendar.MINUTE
-                )
-                return text
-            }
-            else -> return ""
-        }
-
-    }
-
-     */
 }

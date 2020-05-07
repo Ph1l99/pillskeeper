f1023b75e95ceb8067822a4cb91ab82e5eccd315
phil <pippo.coglio@gmail.com>
2020-05-07 06:54:39 +0200

    Fixed build.gradle

--- app/src/test/java/com/pillskeeper/ExampleUnitTest.kt
+++ app/src/test/java/com/pillskeeper/ExampleUnitTest.kt
@@ -3,9 +3,6 @@ package com.pillskeeper
 
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.powermock.api.mockito.PowerMockito
 
 /**
  * Example local unit test, which will execute on the development machine (host).
